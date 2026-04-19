93f7868e4d095ad5929809b0ab780b271096a145
Andry <andryr@users.noreply.github.com>
2026-04-19 19:20:42 -0400

    Add: my-calendar-i - submission-0

---
+++ Data Structures & Algorithms/my-calendar-i/submission-0.py
@@ -0,0 +1,21 @@
+import bisect
+from sortedcontainers import SortedList
+class MyCalendar:
+    
+    def __init__(self):
+        self.events = SortedList()
+
+    def book(self, startTime: int, endTime: int) -> bool:
+        event = (startTime, endTime)
+        idx = bisect.bisect_left(self.events, event)
+        if idx > 0 and self.events[idx - 1][1] > startTime:
+            return False
+        if idx < len(self.events) and self.events[idx][0] < endTime:
+            return False
+        self.events.add((startTime, endTime))
+        return True
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(startTime,endTime)
