82c82d4d586d076e13e2b79c330cdd08fcd5df62
Andry <andryr@users.noreply.github.com>
2026-05-02 19:23:44 -0400

    Add: single-number-iii - submission-1

---
+++ Data Structures & Algorithms/single-number-iii/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+        for num in nums:
+            xor ^= num
+
+        b = xor & (-xor)
+        
+        a = 0
+        for num in nums:
+            if num & b:
+                a ^= num
+
+        return [a, xor ^ a]
